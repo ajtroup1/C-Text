9eb939df0ab2cf33925643f5832e2afc7c5ddacc
Adam Troup <ajtroup1@crimson.ua.edu>
2024-12-23 21:21:57 -0600

    many many themes

--- C-Text/src/renderer/src/types/Settings.d.tsx
+++ C-Text/src/renderer/src/types/Settings.d.tsx
@@ -11,7 +11,64 @@ export interface Settings {
     fontSize: number // EDITOR font size, not the general font size
     highlightCurrentLine: boolean
     lineNumbers: boolean
-    theme: 'light_1' | 'dark_1' | 'light_2' | "dark_2"
+    theme:
+      | 'light_1'
+      | 'dark_1'
+      | 'light_2'
+      | 'dark_2'
+      | 'monokai'
+      | 'nord'
+      | 'solarized-dark'
+      | 'twilight'
+      | 'active4d'
+      | 'all-hallows-eve'
+      | 'amy'
+      | 'birds-of-paradise'
+      | 'blackboard'
+      | 'brilliance-black'
+      | 'brilliance-dull'
+      | 'chrome-devtools'
+      | 'clouds-midnight'
+      | 'clouds'
+      | 'cobalt'
+      | 'cobalt2'
+      | 'dawn'
+      | 'dominion-day'
+      | 'dracula'
+      | 'dreamweaver'
+      | 'eiffel'
+      | 'espresso-libre'
+      | 'github-dark'
+      | 'github-light'
+      | 'github'
+      | 'idle'
+      | 'idlefingers'
+      | 'iplastic'
+      | 'katzenmilch'
+      | 'krtheme'
+      | 'kuroir-theme'
+      | 'lazy'
+      | 'magicwb'
+      | 'merbivore'
+      | 'merbivore-soft'
+      | 'monoindustrial'
+      | 'monokai-bright'
+      | 'night-owl'
+      | 'oceanic-next'
+      | 'pastels-on-dark'
+      | 'slush-and-poppies'
+      | 'solarized-light'
+      | 'space-cadet'
+      | 'sunburst'
+      | 'textmate'
+      | 'tomorrow-night-blue'
+      | 'tomorrow-night-bright'
+      | 'tomorrow-night-eighties'
+      | 'tomorrow-night'
+      | 'tomorrow'
+      | 'upstream-sunburst'
+      | 'vibrant-ink'
+      | 'zenburnesque'
   }
   defaultWorkspace: {
     path: string
@@ -25,38 +82,34 @@ export interface Settings {
 }
 
 export type ToggleConfig = {
-  type: 'toggle';
-  label: string;
-  desc?: string;
-};
+  type: 'toggle'
+  label: string
+  desc?: string
+}
 
 export type DropdownConfig = {
-  type: 'dropdown';
-  label: string;
-  options: string[];
-  desc?: string;
-};
+  type: 'dropdown'
+  label: string
+  options: string[]
+  desc?: string
+}
 
 export type NumberConfig = {
-  type: 'number';
-  label: string;
-  desc?: string;
-};
+  type: 'number'
+  label: string
+  desc?: string
+}
 
 export type InputConfig = {
-  type: 'input';
-  label: string;
-  desc?: string;
-};
+  type: 'input'
+  label: string
+  desc?: string
+}
 
-export type SettingField = 
-  | ToggleConfig
-  | DropdownConfig
-  | NumberConfig
-  | InputConfig;
+export type SettingField = ToggleConfig | DropdownConfig | NumberConfig | InputConfig
 
 export type SettingsMetadata = {
   [category: string]: {
-    [key: string]: SettingField;
-  };
-};
+    [key: string]: SettingField
+  }
+}
